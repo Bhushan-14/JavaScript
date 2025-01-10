e6dd6bf04d221d5f333f7de11c448642efc063b6
Bhushan Patil <139554602+Bhushan-14@users.noreply.github.com>
2025-01-10 04:34:07 +0000

    commit successfully

---
+++ 01_Basics/comparisions.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 2);
+// console.log(2 != 1);
+
+// console.log(2 === 1);
+
+// console.log(2 < "2");
+// console.log(2 > "2");
+// console.log(2 === "2");
+// console.log(2 == "2");
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null === 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+
+/*
+    ==  => value
+    
+    Strict Check => '===' => value and type
+*/ 
+
+// console.log(2 == "ag");
