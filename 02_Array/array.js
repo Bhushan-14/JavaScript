626d39c46dd6231830d79324d332ea649b0fb7b4
Bhushan Patil <139554602+Bhushan-14@users.noreply.github.com>
2025-01-11 03:51:59 +0000

    success

--- 02_Array/array.js
+++ 02_Array/array.js
@@ -1,12 +1,44 @@
 let fruit = ["Mango", "Banana", "Apple", "Orange", "Pineapple"];
 let mixArray =[1,"Sagar", "Mayur", "Hrishi", true, 4, 5];
 
-console.log(fruit[0]);
-console.log(mixArray[5]);
+// console.log(fruit[0]);
+// console.log(mixArray[5]);
 
-mixArray.push(3);
+// mixArray.push(3);
+// console.log(mixArray);
+// console.log(mixArray.length)
+
+// console.log(mixArray.pop());
+// console.log(mixArray.length);
+
+
+// mixArray.unshift(6) //To much time consuming opertaion
+// console.log(mixArray);
+
+// mixArray.shift()
+// console.log(mixArray)
+
+// console.log(mixArray.includes('Sagar')) //boolean output
+
+// console.log(mixArray.indexOf(4))
+
+// const joinArray = mixArray.join();
+// console.log(joinArray)
+// console.log(typeof joinArray);
+
+
+//slice, splice
 console.log(mixArray);
 
-console.log(mixArray.pop());
+const myn1 = mixArray.slice(1, 3); 
+console.log(myn1);
+console.log(mixArray)
 
-console.log(mixArray.length);
+const myn2 = mixArray.splice(1, 3);
+console.log(myn2);
+console.log(mixArray)
+
+/*
+    - Slice operations excludes the rang while keeping original array as it it
+    - Splice
+*/
