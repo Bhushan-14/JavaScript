16e08410fc4f44bf7cc35171e05cfc0b9a1e515f
Bhushan Patil <139554602+Bhushan-14@users.noreply.github.com>
2025-01-11 12:52:44 +0000

    Success

---
+++ 04_Functions/01_functions.js
@@ -0,0 +1,26 @@
+/*
+    Functions:
+    - Functions are a block of code that can be reused.
+
+*/
+
+// function sayMyName(){
+//     console.log("s");
+//     console.log("A");
+//     console.log("G");
+//     console.log("A");
+//     console.log("R");    
+// }
+
+// sayMyName();
+
+// function addTwoNumbers(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.log(`Result of Addition of two numbers: ${addTwoNumbers(3, 5)}`);
+
+function loginUserMessage(userName){
+    return `${userName} Welcome!`;
+}
+console.log(loginUserMessage("Sagar"))
---
+++ 04_Functions/02_functions.js
@@ -0,0 +1,39 @@
+/*
+    In such situations where we don't know how much arguments will be added in the function, we can use the rest operator.
+*/
+
+// let total = 0;
+// function calculateCartPrice(...num){
+//     for(let price of num){
+//         total = total + price;
+//     }
+//     return total;
+// }
+
+// console.log(calculateCartPrice(100,200,300,400));
+
+// const user = {
+//     username : "Sagar",
+//     age : 25,
+// }
+
+// function handleObject(userObj){
+//     console.log(`Username: ${userObj.username} and Age: ${userObj.age}`);
+// }   
+
+// handleObject(user);
+
+// handleObject({
+//     username : "Sagar",
+//     age : 24,
+//     income : 244,
+// });
+
+
+// const newArr = [200, 300, 400, 500, 600, 700];
+// function handleArray(arr){
+//     return arr[2];
+// }
+// console.log(handleArray(newArr));
+// console.log(handleArray([100, 200, 300, 400, 500, 600, 700]));
+
